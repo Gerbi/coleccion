a53301175335ec00e6862b1261138e167320af80
Gerbi <gerbi@users.noreply.github.com>
2020-04-09 19:52:23 -0500

    add Footer

---
+++ src/components/Fitness/Footer.vue
@@ -0,0 +1,132 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <footer class="relative pt-8 pb-6 bg-teal-600">
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-wrap h-full">
+        <div class="flex flex-col justify-between h-full px-4 lg:w-5/12">
+          <div>
+            <a href="/" class="mr-8 text-2xl font-bold uppercase">
+              <span class="text-white">Fitne</span>
+              <span class="text-orange-600">ss</span>
+            </a>
+            <h5 class="mt-0 mb-2 text-gray-100 text-md">
+              Find us on any of these platforms, we respond 1-2 business days.
+            </h5>
+            <div class="mt-6">
+            </div>
+            <button
+              class="items-center justify-center w-10 h-10 mr-2 font-normal text-blue-400 bg-white rounded-full shadow-lg outline-none align-center focus:outline-none"
+              type="button"
+            >
+              <i class="fab fa-twitter"></i></button
+            ><button
+              class="items-center justify-center w-10 h-10 mr-2 font-normal text-blue-600 bg-white rounded-full shadow-lg outline-none align-center focus:outline-none"
+              type="button"
+            >
+              <i class="fab fa-facebook-square"></i></button
+            >
+            <button
+              class="items-center justify-center w-10 h-10 mr-2 font-normal text-gray-900 bg-white rounded-full shadow-lg outline-none align-center focus:outline-none"
+              type="button"
+            >
+              <i class="fab fa-github"></i>
+            </button>
+          </div>
+          <form class="mt-6 lg:mt-20">
+            <div class="flex flex-col overflow-hidden rounded-sm sm:flex-row">
+              <input class="w-full px-4 py-3 text-gray-800 placeholder-gray-500 bg-gray-200 border-2 border-gray-300 outline-none focus:bg-gray-100" type="text" name="email" placeholder="Enter your email">
+              <button class="px-4 py-3 font-semibold text-white uppercase bg-orange-500 hover:bg-orange-600">subscribe</button>
+            </div>
+          </form>
+        </div>
+        <div class="w-full lg:w-7/12">
+          <div class="flex flex-wrap mb-6 items-top">
+            <div class="w-full pt-6 lg:pt-0 px-4 md:w-1/2">
+              <span
+                class="block mb-4 text-sm font-semibold text-white uppercase"
+                >blogs</span
+              >
+              <ul class="list-unstyled">
+                <li class="my-2">
+                  <a
+                    class="flex block pb-2 text-sm font-semibold text-gray-200  hover:text-gray-900"
+                    href="https://www.creative-tim.com/presentation"
+                    >
+                    <span class="w-40 h-16 overflow-hidden">
+                      <img class="w-full h-full" src="https://images.unsplash.com/photo-1586373182275-4a69f12ffa12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="">
+                    </span>
+                    <span class="ml-3 text-sm leading-relaxed break-words">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+                  </a>
+                </li>
+                <li class="my-2">
+                  <a
+                    class="flex block pb-2 text-sm font-semibold text-gray-200  hover:text-gray-900"
+                    href="https://www.creative-tim.com/presentation"
+                  >
+                    <span class="w-40 h-16 overflow-hidden">
+                      <img class="w-full h-full" src="https://images.unsplash.com/photo-1586365967509-c69bbaf69b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60" alt="">
+                    </span>
+                    <span class="ml-3 text-sm leading-relaxed break-words">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+                  </a>
+                </li>
+                <li class="my-2">
+                  <a
+                    class="flex block pb-2 text-sm font-semibold text-gray-200  hover:text-gray-900"
+                    href="https://www.creative-tim.com/presentation"
+                  >
+                    <span class="w-40 h-16 overflow-hidden">
+                      <img class="w-full h-full" src="https://images.unsplash.com/photo-1586320902477-24c19029bd4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80" alt="">
+                    </span>
+                    <span class="ml-3 text-sm leading-relaxed break-words">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="w-full pt-6 lg:pt-0 px-4 md:w-1/2">
+              <span
+                class="block mb-4 text-sm font-semibold text-white uppercase"
+                >instagram</span
+              >
+              <div class="grid grid-cols-3 gap-1 mb-2">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1586397148583-f5a6db7dff06?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1586397148051-54eb8750f0dc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1564001388515-0389ca5b2bd1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1586365967509-c69bbaf69b23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=60" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1559716780-93b994ba903e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1527986463121-ef799810ea24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1551463731-fbd292f1bf03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1539768942893-daf53e448371?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=351&q=80" alt="">
+                <img class="object-cover w-full h-24" src="https://images.unsplash.com/photo-1528195175417-3c2cc4e61e8b?ixlib=rb-1.2.1&auto=format&fit=crop&w=350&q=80" alt="">
+              </div>
+              <a href="#" class="mt-4 text-sm text-white">View more photos</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="mt-2 mb-6 border-gray-400" />
+      <div
+        class="flex flex-wrap items-center justify-center md:justify-between"
+      >
+        <div class="w-full px-4 mx-auto text-center md:w-4/12">
+          <div class="py-1 text-sm font-semibold text-gray-200">
+            Copyright © {{date}} Example Kit by
+            <a
+              href="https://www.github.com/gerbi"
+              class="text-gray-200 hover:text-gray-800"
+              >Gerbidev</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
+};
+</script>
