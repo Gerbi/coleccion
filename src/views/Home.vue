b48581fe120050531bec2e8d6f8373fd17e111b2
Gerbi <gerbi@users.noreply.github.com>
2020-04-16 15:23:47 -0600

    Fix main

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- eslint-disable max-len -->
+  <main class="flex items-center justify-center min-h-screen antialiased leading-relaxed">
+    <ul class="flex flex-col">
+      <li>
+        <router-link to="/fitness">Example 01</router-link>
+      </li>
+      <li>
+        <router-link to="/example02">Example 02</router-link>
+      </li>
+      <li>
+        <router-link to="/example03">Example 03</router-link>
+      </li>
+    </ul>
+  </main>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: 'landing-page',
+  data() {
+    return {
+      // eslint-disable-next-line global-require
+      img: require('../assets/img/1.jpg'),
+      // eslint-disable-next-line global-require
+      imgs: require('../assets/img/1.jpg'),
+      // eslint-disable-next-line global-require
+      trainer: require('../assets/img/personal_traiiner.webp'),
+    };
   },
 };
 </script>
