0463b16c32e32cc3ec28382dcd3dc03db8cb4b9f
Gerbi <gerbi@users.noreply.github.com>
2020-04-16 00:54:23 -0600

    03 Iframe

---
+++ src/views/Example03/Iframe.vue
@@ -0,0 +1,82 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div class="flex items-center justify-center my-8">
+    <div class="border rounded-md sm:w-2/3 md:w-1/2">
+      <div>
+        <a href="#" class="block transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:bg-gray-50">
+            <div class="flex p-2">
+              <div class="flex items-center flex-1 min-w-0">
+                <div class="flex-shrink-0">
+                  <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                </div>
+                <div class="flex-1 min-w-0 px-2 md:grid md:grid-cols-2 md:gap-2">
+                  <div>
+                    <div class="text-sm font-bold leading-5 text-blue-600 truncate">Ricardo Cooper</div>
+                    <div class="flex items-center text-sm leading-5 text-gray-500">
+                      <span class="text-xs truncate">9.9m follower</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button class="px-2 py-1 text-xs text-white rounded-sm bg-blue-500">View Profile</button>
+              </div>
+            </div>
+          </a>
+      </div>
+      <div class="w-full">
+        <img src="https://images.unsplash.com/photo-1565462905102-140e712045aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="">
+      </div>
+      <div class="px-4 my-2 text-md font-medium text-blue-500">View More</div>
+      <hr>
+      <div class="flex justify-between px-4 my-2">
+        <div class="flex">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-7 h-7 mr-1 text-gray-700">
+            <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+          </svg>
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-7 h-7 mx-1 text-gray-700">
+            <path d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
+          </svg>
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-7 h-7 mx-1 text-gray-700">
+            <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
+          </svg>
+        </div>
+        <div>
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8">
+            <path d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path>
+          </svg>
+        </div>
+      </div>
+      <div class="px-4">
+        <p class="text-gray-800 font-bold text-md">243,997 likes</p>
+        <p class="text-sm text-black font-bold">wikipedia</p>
+      </div>
+      <div class="px-4 pt-1 pb-2">
+        <p class="text-sm text-gray-800 font-normal leading-relaxed">Verified
+          Oh, like you never named your kid Genital fissure!!!!!!!</p>
+        <p class="text-xs mt-1 text-gray-600 leading-snug">view all 3,522 comments</p>
+      </div>
+      <hr>
+      <div class="flex items-center justify-between pl-4 py-2">
+        <div class="text-gray-600 w-full">
+          <input type="text" class="w-full" placeholder="Add a comment">
+        </div>
+        <div class="mx-4">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-7 h-7 text-gray-800">
+            <path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Iframe',
+};
+</script>
+
+<style scoped>
+
+</style>
