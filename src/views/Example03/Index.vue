316297f6f8c4dfcc59b1f7ebde2ec9945ff811c4
Gerbi <gerbi@users.noreply.github.com>
2020-04-16 15:13:28 -0600

    03 Index

---
+++ src/views/Example03/Index.vue
@@ -0,0 +1,149 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <div class="bg-gray-200 text-gray-900 antialiased font-sans">
+    <div>
+      <Responsive />
+      <Top />
+      <div class="pt-5 sm:pt-0"></div>
+      <main class="pt-40" style="font-family: Roboto">
+        <div class="max-w-6xl mx-auto px-2 sm:px-4 md:px-6 lg:px-8">
+          <div class="bg-white py-6 sm:py-10 lg:py-16 shadow overflow-hidden sm:rounded-md">
+            <header class="px-5 md:px-16 lg:px-16 xl:px-24" style="font-family: Roboto">
+              <div class="font-sans text-xl text-indigo-700 leading-relaxed">After almost three years of blocking</div>
+              <p class="text-4xl md:text-5xl font-sans font-extrabold leading-tight text-gray-800">Wikipedia, it is accessible again in Turkey</p>
+              <div class="mt-3 text-xl font-normal">
+                <p>After 991 days, the online encyclopedia Wikipedia has been blocked in Turkey. The decision came due to a holiday for operators and fans of the lexicon.</p>
+              </div>
+              <div class="mt-4">
+                <span class="mr-2 text-gray-700">By</span>
+                <a href="#" class="text-black border-b border-gray-600 hover:border-black">Alexander Sar</a>
+              </div>
+              <div>
+                <span class="text-sm font-medium text-gray-600">
+                  <time datetime="06.02.2020:18:41">06.02.2020, 18:41 UTC</time>
+                </span>
+              </div>
+            </header>
+            <div class="absolute relative top-0 z-20 w-full overflow-x-hidden" style="height: calc(100% - 100vh);">
+              <div class="sticky px-5 md:px-16 lg:px-10">
+                <div class="relative my-4" style="background-image: linear-gradient(90deg,#807e7c 25%,rgba(128,126,124,0) 0);
+                background-position: 0 100%;
+                background-size: 4px 1px;
+                background-repeat: repeat-x;
+                -ms-interpolation-mode: nearest-neighbor;
+                image-rendering: pixelated;">
+                  <div class="flex items-center justify-between py-3" style="background-image: linear-gradient(90deg,#807e7c 25%,rgba(128,126,124,0) 0);
+                  background-size: 4px 1px;
+                  background-repeat: repeat-x;
+                  background-repeat-x: repeat;
+                  background-repeat-y: no-repeat;
+                  -ms-interpolation-mode: nearest-neighbor;
+                  image-rendering: pixelated;">
+                    <ul class="flex items-center -mx-2">
+                      <li class="mx-2">
+                        <a href="/news">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                          </svg>
+                        </a>
+                      </li>
+                      <li class="mx-2">
+                        <a href="/msg">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
+                          </svg>
+                        </a>
+                      </li>
+                    </ul>
+                    <ul class="flex items-center -mx-2">
+                      <li class="mx-3">
+                        <a class="flex items-center" href="/msg">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
+                          </svg>
+                        </a>
+                      </li>
+                      <li class="mx-3">
+                        <a class="flex items-center" href="/msg">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"></path>
+                          </svg>
+                        </a>
+                      </li>
+                      <li class="mx-3">
+                        <a class="flex items-center" href="/msg">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                          </svg>
+                        </a>
+                      </li>
+                      <li class="hidden mx-3 md:block">
+                        <a class="flex items-center" href="/msg">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path>
+                          </svg>
+                        </a>
+                      </li>
+                      <li class="ml-1 md:hidden" style="background-position: 100%;background-image: linear-gradient(#807e7c 25%,rgba(128,126,124,0) 0);
+                      background-size: 1px 4px;
+                      background-repeat: repeat-y;
+                      -ms-interpolation-mode: nearest-neighbor;
+                      image-rendering: pixelated;width: 1px;height: 1.5rem;">
+                      </li>
+                      <li class="mx-3 md:hidden">
+                        <a class="flex items-center" href="#share">
+                          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24" class="w-8 h-8 text-gray-700">
+                            <path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path>
+                          </svg>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <figure class="mb-4 xl:px-10">
+                <img class="block object-cover mx-auto w-full md:h-full" src="https://cdn.prod.www.spiegel.de/images/c0df827a-00df-4fd8-a19a-7005f2096e36_w948_r1.77_fpx52.08_fpy50.jpg" alt="">
+                <figcaption class="mt-2 text-center">
+                  <span class="block text-md font-medium text-gray-700">Türkisches Wikipedia: Seit Mittwoch wieder normal abrufbar</span>
+                  <span class="block text-xs text-gray-600">Abdurrahman Antakyali/ Depo Photos/ imago images</span>
+                </figcaption>
+              </figure>
+              <div class="px-4 xl:px-24 md:px-16" style="font-family: Roboto">
+                <p class="mb-6 text-md sm:text-lg lg:text-xl leading-normal">Nach fast drei Jahren hat ein Gericht in Ankara die Sperrung des Onlinelexikons Wikipedia in der Türkei aufgehoben. Die Entscheidung wurde am Mittwoch bekannt gegeben, der zugleich den 19. Geburtstag des Mitmach-Lexikons markiert - den sogenannten Wikipedia-Tag.</p>
+                <p class="mb-6 text-md sm:text-lg lg:text-xl leading-normal">Die türkischen Behörden hatten die Online-Enzyklopädie im April 2017 wegen Artikeln blockiert, welche die Regierung in Verbindung mit Extremistengruppen brachten. Vergangenen Monat urteilte das türkische Verfassungsgericht jedoch, dass das Verbot gegen das Recht auf freie Meinungsäußerung verstoße.</p>
+                <p class="mb-6 text-md sm:text-lg lg:text-xl leading-normal">Nachdem das Gericht die Aufhebung der Sperre angekündigt hatte, war Wikipedia nicht sofort für jeden türkischen Internetnutzer zugänglich. Im Laufe des Mittwochs jedoch wurde das Angebot nach Angaben der Website NetBlocks.org für immer mehr Nutzer im Land wieder erreichbar - nach insgesamt 991 Tagen Sperre seit dem 29. April 2017. Die Türkei war neben China das einzige Land weltweit, das Wikipedia komplett gesperrt hatte.</p>
+                <Iframe />
+                <p class="mb-6 text-md sm:text-lg lg:text-xl leading-normal">After almost three years, a court in Ankara lifted the blocking of the online dictionary Wikipedia in Turkey. The decision was announced on Wednesday, which also marks the 19th birthday of the interactive lexicon - the so-called Wikipedia Day.</p>
+                <p class="mb-6 text-md sm:text-lg lg:text-xl leading-normal">After almost three years, a court in Ankara lifted the blocking of the online dictionary Wikipedia in Turkey. The decision was announced on Wednesday, which also marks the 19th birthday of the interactive lexicon - the so-called Wikipedia Day.</p>
+                <p class="mb-6 text-md sm:text-lg lg:text-xl leading-normal">After almost three years, a court in Ankara lifted the blocking of the online dictionary Wikipedia in Turkey. The decision was announced on Wednesday, which also marks the 19th birthday of the interactive lexicon - the so-called Wikipedia Day.</p>
+                <p class="text-md font-medium leading-normal text-gray-600">APF/DP</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="md:mb-6 mb-4"></div>
+        <Related />
+      </main>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Responsive from '../../components/Responsive.vue';
+import Top from './Top.vue';
+import Related from './Related.vue';
+import Footer from './Footer.vue';
+import Iframe from './Iframe.vue';
+
+export default {
+  name: 'Articles',
+  components: {
+    Iframe,
+    Responsive,
+    Top,
+    Footer,
+    Related,
+  },
+};
+</script>
