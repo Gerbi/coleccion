e8f9c9de168bbc9bfb9c9567513967b4e90fba2d
Gerbi <gerbi@users.noreply.github.com>
2020-04-15 22:44:01 -0600

    03-Footer

---
+++ src/views/Example03/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- eslint-disable max-len -->
+  <footer>
+    <div class="py-6 mt-2 text-white bg-black">
+      <div class="flex justify-center py-4">
+        <a href="#" class="mx-3 font-sans text-sm font-medium text-gray-300">Privacy</a>
+        <a href="#" class="mx-3 font-sans text-sm font-medium text-gray-300">Policy Terms of Use</a>
+        <a href="#" class="mx-3 font-sans text-sm font-medium text-gray-300">Contact</a>
+        <a href="#" class="mx-3 font-sans text-sm font-medium text-gray-300">Help</a>
+      </div>
+      <div class="flex items-center justify-center">
+        <a href="#" class="mx-4 font-sans text-sm font-medium text-gray-300">Twitter</a>
+        <a href="#" class="mx-4 font-sans text-sm font-medium text-gray-300">Telegram</a>
+        <a href="#" class="mx-4 font-sans text-sm font-medium text-gray-300">Facebook</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+
+</style>
